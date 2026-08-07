5ff19de5bf23596c5c74416802a7e119eedfe208
Casey Dettlaff <casey.dettlaff@gmail.com>
2026-08-07 13:46:49 -0700

    Normalized Training Data
    Adjusted create_samples function so that we get P and Q in per-unit, and get Theta in radians.

--- data_generation.py
+++ data_generation.py
@@ -40,6 +40,7 @@ def perturb_lines(net, r_var=0.1, x_var=0.1):
 def create_sample(net):
     
     pp.runpp(net)
+    S_base = net.sn_mva
     
     # Build node features x = [P, V, Q, theta, mP, mV, mQ, mTheta]
     n = len(net.bus)
@@ -47,8 +48,8 @@ def create_sample(net):
     
     # PQ buses
     for idx in net.load.bus:
-        p = net.load.loc[net.load.bus == idx, 'p_mw'].sum()
-        q = net.load.loc[net.load.bus == idx, 'q_mvar'].sum()
+        p = net.load.loc[net.load.bus == idx, 'p_mw'].sum() / S_base
+        q = net.load.loc[net.load.bus == idx, 'q_mvar'].sum() / S_base
         x[idx] = torch.tensor([
             p,
             0.0,
@@ -59,11 +60,11 @@ def create_sample(net):
     # PV buses
     for _, gen in net.gen.iterrows():
         x[int(gen.bus)] = torch.tensor([
-            gen.p_mw,
+            gen.p_mw / S_base,
             gen.vm_pu,
             0.0,
             0.0,
-            1,1,0,0])
+            1, 1, 0, 0])
         
     # Slack bus
     for _, ext in net.ext_grid.iterrows():
@@ -71,69 +72,40 @@ def create_sample(net):
             0.0,
             ext.vm_pu,
             0.0,
-            ext.va_degree,
-            0,1,0,1])
+            np.deg2rad(ext.va_degree),
+            0, 1, 0, 1])
         
-    # Edge List
-
+    # Edge list
     edge_index = torch.tensor([
         net.line.from_bus.values,
         net.line.to_bus.values,
-        ], dtype=torch.long)
-
+    ], dtype=torch.long)
+    
     edge_attr = torch.tensor(
         net.line[['r_ohm_per_km', 'x_ohm_per_km']].values,
         dtype=torch.float)
-
-    # Make graph undirected
+    
     edge_index = torch.cat([edge_index, edge_index.flip(0)], dim=1)
     edge_attr = torch.cat([edge_attr, edge_attr], dim=0)
-
-    # Targets
-    # y = [V, theta, P, Q]
-
-    res = net.res_bus
+    
+    # Targets: [V, theta, P, Q]
     y = torch.tensor(
-        res[['vm_pu', 'va_degree', 'p_mw', 'q_mvar']].values,
+        np.column_stack([
+            net.res_bus.vm_pu.values,
+            np.deg2rad(net.res_bus.va_degree.values),
+            net.res_bus.p_mw.values / S_base,
+            net.res_bus.q_mvar.values / S_base]),
         dtype=torch.float)
-
-    # PyTorch Geometric Object
-    data = Data(
-        x=x,
-        edge_index=edge_index,
-        edge_attr=edge_attr,
-        y=y)
-
-    return data
-
-def generate_dataset(
-    net,
-    n_samples,
-    vary_loads=True,
-    vary_lines=True,
-    global_scale=(0.7,1.3),
-    local_var=0.1,
-    r_var=0.1,
-    x_var=0.1):
     
-    dataset = []
-    
-    for _ in tqdm(range(n_samples), desc="Generating samples"):
-        
-        net_i = copy.deepcopy(net)
-        
-        if vary_loads:
-            net_i = perturb_loads(
-                net_i,
-                global_scale=np.random.uniform(*global_scale),
-                local_var=local_var)
-        if vary_lines:
-            net_i = perturb_lines(
-                net_i,
-                r_var=r_var,
-                x_var=x_var)
-        dataset.append(create_sample(net_i))
-    return dataset
+    # Targets: [V, theta, P, Q]
+    y = torch.tensor(
+        np.column_stack([
+            net.res_bus.vm_pu.values,
+            np.deg2rad(net.res_bus.va_degree.values),
+            net.res_bus.p_mw.values / S_base,
+            net.res_bus.q_mvar.values / S_base
+        ]),
+        dtype=torch.float)
 
 def train_test_split(dataset, train_ratio=0.8):
     
