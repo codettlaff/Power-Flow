620fb13dde0e147529a52a0d0aa1a75e07b90205
Casey Dettlaff <casey.dettlaff@gmail.com>
2026-08-20 16:10:29 -0700

    use intermediate global scale range

--- data_generation.py
+++ data_generation.py
@@ -183,7 +183,7 @@ if __name__ == '__main__':
     test_data_filepath = os.path.join(data_dir, 'case14_test2.npy')
     
     n_samples = int(5e3)
-    scale_range = (0.1, 2)
+    scale_range = (0.4, 1.6)
     
     net = pn.case14()
     dataset = generate_dataset(net, n_samples, scale_range=scale_range)
